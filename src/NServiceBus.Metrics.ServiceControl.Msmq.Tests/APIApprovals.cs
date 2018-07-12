7c7042cac0e3a2f216633a8fd95c94d80e54bf45
Ramon Smits <ramon.smits@gmail.com>
2018-07-12 23:34:35 +0200

    Packaging, API Approvals & cleanup

---
+++ src/NServiceBus.Metrics.ServiceControl.Msmq.Tests/APIApprovals.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Particular.Approvals;
+using PublicApiGenerator;
+
+[TestFixture]
+public class APIApprovals
+{
+    [Test]
+    public void Approve()
+    {
+        var publicApi = ApiGenerator.GeneratePublicApi(typeof(ReportMsmqNativeQueueLength).Assembly);
+        Approver.Verify(publicApi);
+    }
+}
---
+++ src/NServiceBus.Metrics.ServiceControl.Msmq/InternalsVisibleTo.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("NServiceBus.Metrics.ServiceControl.Msmq.Tests")]
