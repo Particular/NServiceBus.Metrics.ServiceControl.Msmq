61768ab187cab61cfb4ca08612dddb1baf3f10e5
David Boike <david.boike@gmail.com>
2021-11-09 14:49:57 -0600

    APIApproval to ignore AssemblyMetadataAttribute

--- src/NServiceBus.Metrics.ServiceControl..Msmq.Tests/APIApprovals.cs
+++ src/NServiceBus.Metrics.ServiceControl..Msmq.Tests/APIApprovals.cs
@@ -8,7 +8,11 @@ public class APIApprovals
     [Test]
     public void Approve()
     {
-        var publicApi = ApiGenerator.GeneratePublicApi(typeof(MsmqExtensions).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+        var publicApi = ApiGenerator.GeneratePublicApi(typeof(MsmqExtensions).Assembly, excludeAttributes: new[]
+        {
+            "System.Runtime.Versioning.TargetFrameworkAttribute",
+            "System.Reflection.AssemblyMetadataAttribute"
+        });
         Approver.Verify(publicApi);
     }
 }
